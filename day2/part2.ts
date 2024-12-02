4846d26bee7d364d72c648b07efa61f05afe8e9d
adamalmqvist <70197981+adamalmqvist@users.noreply.github.com>
2024-12-02 17:48:50 +0100

    solution for day2

---
+++ day2/part2.ts
@@ -0,0 +1,43 @@
+const rawFile = await Deno.readTextFile("input.txt");
+const rows = rawFile.split(/\n/);
+
+let safeCounter = 0;
+for (const row of rows) {
+    const numbers = row.split(' ').map(Number);
+    const isSequenceSafe = isRowSafe(numbers);
+    if (isSequenceSafe) safeCounter++;
+}
+console.log(safeCounter);
+
+function isRowSafe(nums: number[]) {
+    if (isNumbersSafe(nums)) return true;
+    
+    for (let i = 0; i < nums.length; i++) {
+        const modifiedNums = [...nums.slice(0, i), ...nums.slice(i + 1)];
+        if (isNumbersSafe(modifiedNums)) return true;
+    }
+    
+    return false;
+}
+
+function isNumbersSafe(nums: number[]) {
+    if (nums.length <= 1) return false;
+    
+    const isDecreasing = nums[0] > nums[1];
+    
+    for (let i = 1; i < nums.length; i++) {
+        const diff = Math.abs(nums[i] - nums[i-1]);
+        
+        if (isDecreasing) {
+            if (nums[i] > nums[i-1] || nums[i] === nums[i-1] || diff > 3) {
+                return false;
+            }
+        } else {
+            if (nums[i] < nums[i-1] || nums[i] === nums[i-1] || diff > 3) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
